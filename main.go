07bc2fc78ebd0ed78ae58eb6a6b924be4ec2d48c
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-07-28 17:50:34 +0200

    renaming som variables

--- main.go
+++ main.go
@@ -3,40 +3,48 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 )
 
 //Drone holds the data and methods specific for the drone
 type Drone struct {
-	droneAddress     string //The ip address of the drone
-	portDiscover     string //Used for initializing the connection to the drone over TCP
-	portD2C          string //Drone to controller, port the controller will listen on for drone messages
-	portRTPStream    string
-	portRTPControl   string
-	chReceivedPacket chan networkPacket
+	addressDrone        string //The ip address of the drone
+	portDiscover        string //Used for initializing the connection to the drone over TCP
+	portC2D             string //TODO: Make this one to be filled with port value from discover.
+	portD2C             string //Drone to controller, port the controller will listen on for drone messages
+	portRTPStream       string
+	portRTPControl      string
+	chReceivedUDPPacket chan networkUDPPacket
+	chSendingUDPPacket  chan networkUDPPacket
 }
 
 //NewDrone will initalize all the variables needed for a drone,
 // like ports used, ip adresses, etc.
 func NewDrone() *Drone {
 	return &Drone{
-		droneAddress:   "192.168.42.1",
-		portDiscover:   "44444",
+		addressDrone: "192.168.42.1",
+		portDiscover: "44444",
+		//portC2D:        "54321", //This one is now assigned via discovery
 		portD2C:        "43210",
 		portRTPStream:  "55004",
 		portRTPControl: "55005",
 
-		chReceivedPacket: make(chan networkPacket),
-	}
+		chReceivedUDPPacket: make(chan networkUDPPacket),
+		chSendingUDPPacket:  make(chan networkUDPPacket)}
 }
 
 //Discover will initalize the connection with the drone.
+// A discover will JSON formated data like :
+//
+// { "status": 0, "c2d_port": 54321, "c2d_update_port": 51, "c2d_user_port": 21, "qos_mode": 0, "arstream2_server_stream_port": 5004, "arstream2_server_control_port": 5005 }
 func (d *Drone) Discover() error {
 	//const addr = "192.168.42.1:44444"
 
-	discoverConn, err := net.Dial("tcp", d.droneAddress+":"+d.portDiscover)
+	discoverConn, err := net.Dial("tcp", d.addressDrone+":"+d.portDiscover)
 
 	if err != nil {
 		return err
@@ -60,20 +68,47 @@ func (d *Drone) Discover() error {
 		log.Println("error: Discover, discoveryClient.Write: ", err)
 	}
 
-	data := make([]byte, 10240)
+	data := make([]byte, 1024) //not quite sure about the size here...
 
 	_, err = discoverConn.Read(data)
-
 	if err != nil {
 		return err
 	}
+	fmt.Printf("*** Discovery data \n %v \n\n, Size of data = %v\n", string(data), len(data))
+
+	//Using anonymous struct just for unmarshalling the discoveryData
+	discoverData := struct {
+		Status                     int `json:"status"`
+		C2dPort                    int `json:"c2d_port"`
+		C2dUpdate                  int `json:"c2d_update_port"`
+		C2dUserPort                int `json:"c2d_user_port"`
+		QosMode                    int `json:"qos_mode"`
+		Arstream2ServerStreamPort  int `json:"arstream2_server_stream_port"`
+		Arstream2ServerControlPort int `json:"arstream2_server_control_port"`
+	}{}
+
+	//Remove all the zero allocations in the byte slice, else unmarshal will fail.
+	data = bytes.Trim(data, "\x00")
+
+	if err := json.Unmarshal(data, &discoverData); err != nil {
+		log.Println("error:Umarshal discovery data: ", err)
+	}
+	fmt.Println("Unmarshaled : ", discoverData)
+
+	//if the status !=0 the disovery failed.
+	if discoverData.Status != 0 {
+		log.Fatal("DISCOVERY FAILED") //TODO: put in a timer and make it retry after a few seconds.
+	}
+
+	d.portC2D = strconv.Itoa(discoverData.C2dPort)
+	fmt.Printf("portC2D is of type = %T, and value = %s \n", d.portC2D, d.portC2D)
 
 	return discoverConn.Close()
 }
 
-//networkPacket (ARNetworkAL), a network packet is the main UDP packet read from the network.
-// A network packet can contain multiple ARNetwork frames.
-type networkPacket struct {
+//networkPacket is the main UDP packet read from the network.
+// A network packet can contain multiple ARNetworkAL/frames.
+type networkUDPPacket struct {
 	size int
 	data []byte
 }
@@ -81,7 +116,7 @@ type networkPacket struct {
 //getNetworkPacketsD2C gets the raw UDP packets from the drone sent to the controller.
 // Will read the raw UDP packets from the network, and put them on a channel to be
 // picked up by the frame decoder.
-func (d *Drone) readNetworkPacketsD2C() {
+func (d *Drone) readNetworkUDPPacketsD2C() {
 	//create an 'empty' UDP listener.
 	localConn, err := net.ListenPacket("udp", ":43210")
 	if err != nil {
@@ -99,42 +134,72 @@ func (d *Drone) readNetworkPacketsD2C() {
 		}
 		log.Printf("info from readNetworkPacketsD2C: read %v bytes, from %v\n", n, addr)
 
-		p := networkPacket{
+		p := networkUDPPacket{
 			size: n,
 			data: buf,
 		}
 
-		//send the packet received over a channel to later parse out frames.
-		d.chReceivedPacket <- p
+		//send the packet received over a channel to later parse out ARNetworkAL/frames.
+		d.chReceivedUDPPacket <- p
 
 	}
 }
 
-//networkFrame
-// A network frame (ARNetworkAL)looks like this, and in the following order :
-// dataType 1 byte,
-// targetBufferID 1 byte,
-// sequeneNumber 1 Byte,
-// frameSize 4 Bytes (little endian),
-// data n bytes (this is the actual drone data ARNetwork),
-type networkFrame struct {
-	dataType       int
-	targetBufferID int
-	sequenceNR     int
-	size           int
-	data           []byte
+//writeNetworkPacketsC2D writes the raw UDP packets from the controller to the drone.
+// Will receive []byte packet to write on an incomming channel for the function.
+func (d *Drone) writeNetworkPacketsC2D() {
+	//TODO:
+	//Have a channel waiting for incomming network packets to be written to the drone
+	// .........
 }
 
-//decodeNetworkFrame will decode one single frame of the ARNetworkAL protocol.
+//networkFrame
+// A network frame (ARNetworkAL)looks like this, and in the following order :
+// - dataType 1 byte,
+// - targetBufferID 1 byte,
+// - sequeneNumber 1 Byte,
+// - frameSize 4 Bytes (little endian),
+// - data n bytes (this is the actual drone data ARNetwork),
+//
+//	Example of size:
+//	01 ba 27 08000000 42, 02 0b c3 0b000000 12345678
+//  --size 0x08=8byte---, --size 0x0b=11byte--------
+type networkFrame struct {
+	//Data types
+	//• Ack(1): Acknowledgment of previously received data
+	//  To Ack a frame, set type to 1,
+	//  add 128 to the value of the bufferID of the package that requires Ack,
+	//	new unique sequence nr. for the ack buffer,
+	//
+	//• Data(2): Normal data (no ack requested)
+	//• Low latency data(3): Treated as normal data on the network, but are
+	//  given higher priority internally
+	//• Data with ack(4): Data requesting an ack. The receiver must send an
+	//  ack for this data !
+	dataType int
+	//• [0; 9]: Reserved values for ARNetwork internal use.
+	//• [10; 127]: Data buffers.
+	//• [128; 255]: Acknowledge buffers.
+	targetBufferID  int
+	sequenceNR      int
+	size            int
+	dataARNetworkAL []byte
+}
+
+func encodeNetworkFrame(dataType int, targetBufferID int, sequenceNR int, size int, dataARNetworkAL []byte) {
+
+}
+
+//decodeARNetworkFrame will decode one single frame of the ARNetworkAL protocol.
 // NB: If the packet consists of more than one frame eg. the length of the
 // packet is longer than the length of the frame, then the function must be
 // run again
-func decodeNetworkFrame(buf []byte, startPosition int) networkFrame {
+func decodeARNetworkFrame(buf []byte, startPosition int) networkFrame {
 	frame := networkFrame{
-		dataType:       int(buf[0]),
-		targetBufferID: int(buf[1]),
-		sequenceNR:     int(buf[2]),
-		data:           []byte{},
+		dataType:        int(buf[0]),
+		targetBufferID:  int(buf[1]),
+		sequenceNR:      int(buf[2]),
+		dataARNetworkAL: []byte{},
 	}
 
 	var size uint32
@@ -143,9 +208,7 @@ func decodeNetworkFrame(buf []byte, startPosition int) networkFrame {
 		log.Println("error: NewNetworkFrame, binary.Read: ", err)
 	}
 	frame.size = int(size)
-	fmt.Println("Info:decodeNetworkFrame: size of whole frame = ", frame.size+7)
-
-	frame.data = buf[7:frame.size]
+	frame.dataARNetworkAL = buf[7:frame.size]
 
 	return frame
 }
@@ -160,13 +223,25 @@ func main() {
 
 	//Will start the reading of whole UDP packets from the network,
 	// and put them on the chReceivedPacket channel.
-	go drone.readNetworkPacketsD2C()
+	go drone.readNetworkUDPPacketsD2C()
 
 	for {
-		packet := <-drone.chReceivedPacket
+		//Get a packet
+		packet := <-drone.chReceivedUDPPacket
 		fmt.Println("info: main: packet size = ", packet.size)
-		//fmt.Println("info: main: packet data = ", packet.data)
+		fmt.Println("info: main: packet data = ", packet.data[:packet.size])
 
+		frame := decodeARNetworkFrame(packet.data, 0)
+		//• Ack(1): Acknowledgment of previously received data
+		//• Data(2): Normal data (no ack requested)
+		//• Low latency data(3): Treated as normal data on the network, but are
+		//  given higher priority internally
+		//• Data with ack(4): Data requesting an ack. The receiver must send an
+		//  ack for this data !
+		fmt.Println("info: main: frame: data type: ", frame.dataType)
+		fmt.Println("info: main: frame: target buffer id: ", frame.targetBufferID)
+		fmt.Println("info: main: frame: size of whole frame = ", frame.size)
+		fmt.Println("info: main: frame: dataARNetworkAL = ", frame.dataARNetworkAL)
 	}
 
 	//time.Sleep(time.Second * 2)
