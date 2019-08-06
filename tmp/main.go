fa9bc181529d4359de25cacbeed694bf1b0ec0d2
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-08-06 09:01:36 +0200

    made a concept for how to lay out commands vs decode methods

--- tmp/main.go
+++ tmp/main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-)
-
-//2 127 75 23 0 0 0 - 1 4 6 0 137 146 75 60 159 186 239 58 74 247 29 192
-//1, 4, 6, 0, 137, 146, 75, 60, 159, 186, 239, 58, 74, 247, 29, 192
+//var data = []byte{1, 4, 6, 0, 66, 170, 134, 60, 21, 152, 181, 189, 131, 170, 224, 191}
 
 func main() {
-	data := []byte{1, 4, 6, 0, 137, 146, 75, 60, 159, 186, 239, 58, 74, 247, 29, 192}
-	var d uint16
 
-	r := bytes.NewReader(data[2:4])
-
-	err := binary.Read(r, binary.LittleEndian, &d)
-	if err != nil {
-		fmt.Println("error: binary.Read failed: ", err)
-	}
-
-	myVar := int(d)
-
-	fmt.Printf("%v, %b\n", myVar, myVar)
 }
